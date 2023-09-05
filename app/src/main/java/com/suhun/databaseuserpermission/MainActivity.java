9d3f151585a77624cbba2bd647b39dc7e15ac619
suhunlin <lin.suhun@msa.hinet.net>
2023-09-05 19:28:17 +0800

    Add layout

--- app/src/main/java/com/suhun/databaseuserpermission/MainActivity.java
+++ app/src/main/java/com/suhun/databaseuserpermission/MainActivity.java
@@ -2,6 +2,7 @@ package com.suhun.databaseuserpermission;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void selectDateFun(View view){
+
+    }
+
+    public void insertFun(View view){
+
+    }
+
+    public void updateFun(View view){
+
+    }
+
+    public void deleteFun(View view){
+
     }
 }
