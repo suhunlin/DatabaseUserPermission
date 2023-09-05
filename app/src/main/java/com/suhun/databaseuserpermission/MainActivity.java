5fcf29caa60b99fee5355d248c0d55371ee0a089
suhunlin <lin.suhun@msa.hinet.net>
2023-09-05 19:33:00 +0800

    Initial TextView, EditText

--- app/src/main/java/com/suhun/databaseuserpermission/MainActivity.java
+++ app/src/main/java/com/suhun/databaseuserpermission/MainActivity.java
@@ -3,13 +3,27 @@ package com.suhun.databaseuserpermission;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private String tag = MainActivity.class.getSimpleName();
+    private TextView resultLog, birthday;
+    private EditText id, name, tel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView(){
+        resultLog = findViewById(R.id.lid_resultLog);
+        id = findViewById(R.id.lid_idInput);
+        name = findViewById(R.id.lid_nameInput);
+        tel = findViewById(R.id.lid_telInput);
+        birthday = findViewById(R.id.lid_birthdayInput);
     }
 
     public void selectDateFun(View view){
